017c562694784a3ad0dc9b0878d15ff4b19022f0
adolfo nin <ionfiz92@gmail.com>
2022-07-04 22:59:05 -0300

    optimizando proyecto

---
+++ stock.js
@@ -0,0 +1,11 @@
+let stockProductos = [
+    {id: 1, freeshipping: false, nombre: "CDJ 2000nx2", tipo: "CDJ PIONEER", desc: "Standard DJ Equipment", precio: 9000,  img: './img/nexus2000_2.jpg'},
+    {id: 2, freeshipping: true, nombre: "CDJ 2000nx", tipo: "CDJ PIONEER", desc: "Standard DJ Equipment", precio: 8000,  img: './img/nexus2000.jpg'},
+    {id: 3, freeshipping: false, nombre: "Xone 92", tipo: "Allen & Heath Mixer", desc: "Standard DJ Equipment", precio: 7000, img: './img/xone92.jpg'},
+    {id: 4, freeshipping: false, nombre: "DJM 900nx2", tipo: "PIONEER Mixer", desc: "Standard DJ Equipment", precio: 7000,  img: './img/djm900nexus2.jpg'},
+    {id: 5, freeshipping: false, nombre: "Das Vantec Combo", tipo: "Parlantes Vantec", desc: "Standard DJ Equipment", precio: 2200,  img: './img/vantecpack.jpg'},
+    {id: 6, freeshipping: true, nombre: "Das Vantec 215", tipo: "Parlantes Vantec", desc: "Standard DJ Equipment", precio: 17000,  img: './img/ev15ekx.jpg'},
+    {id: 7, freeshipping: true, nombre: "Sl 1200 MKIII", tipo: "Technics Turntables", desc: "Standard DJ Equipment", precio: 8000,  img: './img/sl1200mk3.jpg'},
+    {id: 8, freeshipping: true, nombre: "Funktion-one Resolution", tipo: "Parlantes FUNKTION 1", desc: "Standard DJ Equipment", precio: 40000,  img: './img/funktionone_revo2.jpg'},
+    
+]
